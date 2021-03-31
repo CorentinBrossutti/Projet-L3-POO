9526df9362515b2a85d68611c0ce7a1eb239bece
BERTOLONE--LOPEZ-SERRANO MELVYN p1804487 <melvyn.bertolone--lopez-serrano@etu.univ-lyon1.fr>
2021-03-31 17:02:48 +0200

    Case Unique (dalle v2)

--- Core/src/modele/plateau/CaseUnique.java
+++ Core/src/modele/plateau/CaseUnique.java
@@ -3,11 +3,11 @@ package modele.plateau;
 public class CaseUnique extends EntiteStatique {
     public CaseUnique(Jeu _jeu) {
         super(_jeu);
-        this.vie = true;
+        this.used = false; // La case n'est pas encore traversée
     }
     // getter pour savoir si une case a été utilisée, si utilisée -> return true, sinon return false
     public boolean isUsed(){
-        if(!this.vie)
+        if(this.used)
             return true;
         else
             return false;
@@ -15,15 +15,15 @@ public class CaseUnique extends EntiteStatique {
     // TODO : Gestion capsule - Passage inventaire en paramètre
     @Override
     public boolean traversable(){
-        if(this.isUsed())
+        if(this.isUsed()) // Si la case a déjà été traversée -> elle n'est pas traversable
             return false;
         else
             return true;
     }
 
     private void use(){
-        this.vie = false;
+        this.used = true;
     }
 
-    private boolean vie;
+    private boolean used;
 }
